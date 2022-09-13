c83450b903f049c8818f1878353af2b9441f4b54
MrAmir <aam.phoenix@hotmail.com>
2022-09-13 22:38:14 +0430

    Fix eslint error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,11 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 const getInitialProps = createGetInitialProps();
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
 
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <Html>
--- src/utils/colors.d.ts
+++ src/utils/colors.d.ts
@@ -1,32 +1,8 @@
-export interface colorsRgba {
+export interface ColorsRgba {
   r: number;
   g: number;
   b: number;
   a?: number;
 }
 
-export interface colorsHsva {
-  h: number;
-  v: number;
-  s: number;
-  a?: number;
-}
-
-export namespace colors {
-  function rgbToHex(rgb: colorsRgba): string;
-  function hexToRgb(hex: string): colorsRgba;
-  function hsvToRgb(hsv: colorsHsva): colorsRgba;
-  function rgbToHsv(rgb: colorsRgba): colorsHsva;
-  function textToRgb(color: string): colorsRgba;
-  function lighten(color: string, percent: number): string;
-  function luminosity(color: string | colorsRgba): number;
-  function brightness(color: string | colorsRgba): number;
-  function blend(
-    fgColor: string | colorsRgba,
-    bColor: string | colorsRgba
-  ): string;
-  function changeAlpha(color: string, offset: number): string;
-  function getPaletteColor(colorName: string): string;
-}
-
-export function hexToRgb(hex: string): colorsRgba;
+export function hexToRgb(hex: string): ColorsRgba;
